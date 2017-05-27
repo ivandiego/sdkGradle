ff47a98dafdabdcba9ea790e86ff66f63b8df34e
ivandiego <ivandiego2.0@gmail.com>
2017-05-27 00:53:08 -0300

    primeiro commit

---
+++ app/src/main/java/com/iglu/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iglu.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iglu.sdkgradle.Api;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Api.get();
+    }
+}
---
+++ sdkgradle/src/main/java/com/iglu/sdkgradle/Api.java
@@ -0,0 +1,12 @@
+package com.iglu.sdkgradle;
+
+import android.util.Log;
+
+/**
+ * Created by ivan on 27/05/2017.
+ */
+public class Api {
+    static public void get(){
+        Log.d("FUNCIONA","FUNCIONA");
+    }
+}
